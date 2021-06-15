7527ff8199ca3d1eaff9e82db2c5d8918d36bd78
Stefano <piero.quiroz93@gmail.com>
2021-06-15 13:36:07 -0500

    Review useState hook with rcc

--- class/use-state-hook/src/components/LightSwitch.js
+++ class/use-state-hook/src/components/LightSwitch.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/* import React, { useState } from 'react';
 
 const LightSwitch = () => {
     const [state, setState] = useState({
@@ -17,4 +17,33 @@ const LightSwitch = () => {
     );
 }
 
-export default LightSwitch;
+export default LightSwitch; */
+
+import React, { Component } from 'react'
+
+export default class LightSwitch extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            position:"On"
+        }
+    }
+
+    flipSwitch = () => {
+        if(this.state.position === "On"){
+            this.setState({position:"Off"})
+        } else {
+            this.setState({position:"On"})
+        }
+    }
+
+    render() {
+        return (
+            <fieldset>
+                <p>The light is currently {this.state.position}</p>
+                <button onClick={this.flipSwitch}>Flip Switch</button>      
+            </fieldset>
+        )
+    }
+}
+
