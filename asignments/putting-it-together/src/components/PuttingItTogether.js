e0aeb2cc6063ef25435425249a5dc9734ae75317
Stefano <piero.quiroz93@gmail.com>
2021-06-15 14:18:12 -0500

    Putting-It-Together Assignment with rfc

--- asignments/putting-it-together/src/components/PuttingItTogether.js
+++ asignments/putting-it-together/src/components/PuttingItTogether.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+/* import React, { Component } from 'react'
 
 export default class PuttingItTogether extends Component {
     constructor(props){
@@ -24,4 +24,32 @@ export default class PuttingItTogether extends Component {
             </div>
         )
     }
+} */
+
+
+import React, { useState } from 'react';
+
+const PuttingItTogether = (props) => {
+    const {firstName, lastName, age, hairColor} = props;
+    const [ages, setAges] = useState(age);
+
+    const incrementAge = () => {
+        setAges(ages+1);
+    }
+
+    const decrementAge = () => {
+        setAges(ages-1 );
+    }
+    return (
+        <div>
+            <h2>{lastName}, {firstName}</h2>
+            <p>Age: {ages}</p>
+            <p>Hair Color: {hairColor}</p>
+            <button onClick={incrementAge}>Increase Birthday Button for {firstName} {lastName}</button>
+            <br/>
+            <button onClick={decrementAge}>Decrease Birthday Button for {firstName} {lastName}</button>
+        </div>
+    );
 }
+
+export default PuttingItTogether;
