3e01e5a13b0f4c0fd121953d4dc6fb586b10d7cd
Stefano <piero.quiroz93@gmail.com>
2021-06-15 11:29:50 -0500

    Review ClassComponents

---
+++ class/class-components/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import ClassComponents from './components/ClassComponents'
+function App() {
+  return (
+    <div className="App">
+      <ClassComponents/>
+    </div>
+  );
+}
+
+export default App;
