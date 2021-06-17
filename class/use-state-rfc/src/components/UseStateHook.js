f479bd51eceb214b54d7b2c6942e7cd7577378a7
Stefano <piero.quiroz93@gmail.com>
2021-06-16 20:42:30 -0500

    Review useState-hook with rfc Objects

--- class/use-state-rfc/src/components/UseStateHook.js
+++ class/use-state-rfc/src/components/UseStateHook.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UseStateHook = () => {
+    const [state, setState] = useState({
+        clickCount: 0
+    })
+
+    const clickHandler = () => {
+        console.log(state);
+        setState({
+            clickCount: state.clickCount + 1
+        })
+    }
     return (
         <div>
-            
+            <button onClick={clickHandler}>Click me number: {state.clickCount} </button>
         </div>
     );
 }
