2d10fad67a6676845ba8b0815e1b504df219557c
Stefano <piero.quiroz93@gmail.com>
2021-06-26 17:25:10 -0500

    FormValidation useReducer 75%

--- asignments/form-validation/src/components/FormValidation.js
+++ asignments/form-validation/src/components/FormValidation.js
@@ -1,21 +1,22 @@
 import React, { useReducer } from 'react';
 
 const initialState = {
-    firstName:{
-        value:''
-        //error: null
+    firstName: {
+        value: "",
+        error: null
     },
-    lastName : {
-        value:''
-        //error: null
+    lastName: {
+        value: "",
+        error: null
     },
     email: {
-        value:''
-        //error: null
+        value: "",
+        error: null
     }
 }
 
 const reducer = (state, action) => {
+    
     return {
         ...state,
         [action.type] : action.payload
@@ -32,6 +33,9 @@ const FormValidation = () => {
             type: name,
             payload: value
         })
+        /* if(state.firstName.length > 0 && state.firstName.length < 4){
+            state.errors = "First-name must be at least 4 characters";
+        } */
     }
 
     const onSubmit = (event) => {
@@ -39,23 +43,29 @@ const FormValidation = () => {
         console.log(state);
     }
 
-    /* const firstNameFunc = () => {
-        if(state.firstName.value > 0 &&  )
-    } */
 
     return (
         <form onSubmit={onSubmit}>
             <div className="row">
                 <label htmlFor="firstName">First Name: </label>
                 <input type="text" id="firstName" name="firstName" value={state.firstName.value} onChange={onChange}/>
+                {
+                    state.firstName.error !== null ? <p className="error">{state.firstName.error}</p> : ''
+                }
             </div>
             <div className="row">
                 <label htmlFor="lastName">Last Name: </label>
                 <input type="text" id="lastName" name="lastName" value={state.lastName.value} onChange={onChange}/>
+                {
+                    state.lastName.error !== null ? <p className="error">{state.lastName.error}</p> : ''
+                }
             </div>
             <div className="row">
                 <label htmlFor="email">Email: </label>
                 <input type="email" id="email" name="email" value={state.email.value} onChange={onChange}/>
+                {
+                    state.email.error !== null ? <p className="error">{state.firstName.error}</p> : ''
+                }
             </div>
             <input type="submit" value="Send"/>
         </form>
