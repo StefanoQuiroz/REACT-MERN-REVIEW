f1a0fe906e1c87d4359540a40f7eefff5759da4f
Stefano <piero.quiroz93@gmail.com>
2021-06-28 10:49:31 -0500

    Review Box-generator

--- asignments/box-generator/src/components/Box.jsx
+++ asignments/box-generator/src/components/Box.jsx
@@ -8,8 +8,8 @@ const StyledDiv = styled.div`
     justify-content: center;
     align-items: center;
     margin: 2rem 1rem;
-    width: ${w => w.newWidth+"px" || "200px"};
-    height: ${h => h.newHeight+"px" || "200px"};
+    width: ${props => props.newWidth || "200px"};
+    height: ${props => props.newHeight || "200px"};
     background-color: ${ color => color.newColor || "red"};
 `;
 
@@ -18,8 +18,8 @@ const Box = () => {
     const [state, setState] = useState({
         boxes : [],
         color: "",
-        height: 0,
-        width: 0
+        height: "",
+        width: ""
     })
 
     //Se implementa cada onChangeColor, onChangeHeight y onChangeWidth para cada botón Add
@@ -50,10 +50,10 @@ const Box = () => {
         event.preventDefault();
         //si no aplico el setState cambiandole otro key y qye reciba el state.color, este se cambia automaticamente con el onChange sin necesidad del onSubmit (hacer click en el botón)
         setState({
-            boxes: [...state.boxes, {Color:state.color, Height:state.height, Width:state.width}],
+            boxes: [...state.boxes, {_color:state.color, _height:state.height, _width:state.width}],
             color: "",
-            height: 0,
-            width: 0
+            height: "",
+            width: ""
         })
         //console.log(state.boxes);    
         //console.log(state.boxes);
@@ -63,9 +63,9 @@ const Box = () => {
     const divs = state.boxes.map((item, index) => (
         <StyledDiv 
             key={index}
-            newColor={item.Color}
-            newHeight={item.Height}
-            newWidth={item.Width}
+            newColor={item._color}
+            newHeight={item._height}
+            newWidth={item._width}
         />
     ));
 
@@ -79,12 +79,12 @@ const Box = () => {
                 </div>
                 <div className="row">
                     <label htmlFor="height">Height</label>
-                    <input type="number" id="height" name="height" value={state.height} onChange={onChangeHeight} placeholder="Enter the height of the box"/>
+                    <input type="text" id="height" name="height" value={state.height} onChange={onChangeHeight} placeholder="Enter the height of the box"/>
                     <button type="submit">Add</button>
                 </div>
                 <div className="row">
                     <label htmlFor="width">Width</label>
-                    <input type="number" id="width" name="width" value={state.width} onChange={onChangeWidth} placeholder="Enter the width of the box"/>
+                    <input type="text" id="width" name="width" value={state.width} onChange={onChangeWidth} placeholder="Enter the width of the box"/>
                     <button type="submit">Add</button>
                 </div>
             </form>
