2d8c236150c4f6bdb03e235b2ce7a9b317b4a6e7
Stefano <piero.quiroz93@gmail.com>
2021-06-28 11:56:18 -0500

    Adjustments Box-Generator

--- asignments/box-generator/src/components/Box.jsx
+++ asignments/box-generator/src/components/Box.jsx
@@ -3,27 +3,38 @@ import styled from 'styled-components';
 
 const StyledDiv = styled.div`
     display: inline-block;
-    flex-wrap: nowrap;
+    flex-wrap: wrap;
     flex-direction: row;
     justify-content: center;
     align-items: center;
     margin: 2rem 1rem;
-    width: ${props => props.newWidth || "200px"};
-    height: ${props => props.newHeight || "200px"};
+    width: ${props => props.newWidth+"px" || "300px"};
+    height: ${props => props.newHeight+"px" || "100px"};
     background-color: ${ color => color.newColor || "red"};
 `;
 
 const Box = () => {
+    //
+
     //manage the states
     const [state, setState] = useState({
         boxes : [],
         color: "",
-        height: "",
-        width: ""
+        height: 0,
+        width: 0
     })
 
     //Se implementa cada onChangeColor, onChangeHeight y onChangeWidth para cada botón Add
-    const onChangeColor = (event) => {
+
+    const onChange = (event) => {
+        const {name, value} = event.target;
+        setState({
+            ...state,
+            [name]:value
+        })
+    }
+
+    /* const onChangeColor = (event) => {
         const {value} = event.target;
         setState({
             ...state,
@@ -44,19 +55,18 @@ const Box = () => {
             ...state,
             width: value
         })
-    }
-
+    } */
     const onSubmit = (event) => {
         event.preventDefault();
         //si no aplico el setState cambiandole otro key y qye reciba el state.color, este se cambia automaticamente con el onChange sin necesidad del onSubmit (hacer click en el botón)
         setState({
             boxes: [...state.boxes, {_color:state.color, _height:state.height, _width:state.width}],
             color: "",
-            height: "",
-            width: ""
+            height: 0,
+            width: 0
+            
         })
-        //console.log(state.boxes);    
-        //console.log(state.boxes);
+        console.log(state.boxes);    
         //[pink, green, blue, black]    
     }
 
@@ -74,19 +84,19 @@ const Box = () => {
             <form onSubmit={onSubmit}>
                 <div className="row">
                     <label htmlFor="color">Color</label>
-                    <input type="text" id="color" name="color" value={state.color} onChange={onChangeColor} placeholder="Enter the color of the box"/>
-                    <button type="submit">Add</button>
+                    <input type="text" id="color" name="color" value={state.color} onChange={onChange} placeholder="Enter the color of the box"/>
                 </div>
                 <div className="row">
                     <label htmlFor="height">Height</label>
-                    <input type="text" id="height" name="height" value={state.height} onChange={onChangeHeight} placeholder="Enter the height of the box"/>
-                    <button type="submit">Add</button>
+                    <input type="number" id="height" name="height" value={state.height} onChange={onChange} placeholder="Enter the height of the box"/>
                 </div>
                 <div className="row">
                     <label htmlFor="width">Width</label>
-                    <input type="text" id="width" name="width" value={state.width} onChange={onChangeWidth} placeholder="Enter the width of the box"/>
-                    <button type="submit">Add</button>
+                    <input type="number" id="width" name="width" value={state.width} onChange={onChange} placeholder="Enter the width of the box"/>
                 </div>
+               <div className="button">
+                   <button type="submit">Add</button>
+               </div>
             </form>
             <div className="results">
                 {divs}
