93dd4c7b47439abc468a85d6585bf3d664d50726
Stefano <piero.quiroz93@gmail.com>
2021-06-27 22:28:50 -0500

    Added color property in the object

--- asignments/box-generator/src/components/Box.jsx
+++ asignments/box-generator/src/components/Box.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin: 2rem auto;
+    width: 200px;
+    height: 200px;
+    background-color: ${ color => color.newColor || "red"};
+`;
 
 const Box = () => {
     //manage the states
+    const [state, setState] = useState({
+        color: ""
+    })
+
+    const onChange = (event) => {
+        const {name, value} = event.target;
+        setState({
+            ...state,
+            [name]: value
+        })
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        //si no aplico el setState cambiandole otro key y qye reciba el state.color, este se cambia automaticamente con el onChange sin necesidad del onSubmit (hacer click en el botón)
+        setState({
+            colors: state.color
+        })    
+    }
+
     return (
         <div className="container">
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="row">
                     <label htmlFor="color">Color</label>
-                    <input type="text" id="color"/>
+                    <input type="text" id="color" name="color" value={state.color} onChange={onChange}/>
                     <button type="submit">Add</button>
                 </div>
             </form>
             <div className="results">
-                resultados
+                <StyledDiv newColor={state.colors}/>
             </div>            
         </div>
     );
