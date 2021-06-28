83b2018aff3080f88aa81a2d18825d674233b2f1
Stefano <piero.quiroz93@gmail.com>
2021-06-28 12:40:53 -0500

    Box-Generator finished

--- asignments/box-generator/src/components/Box.jsx
+++ asignments/box-generator/src/components/Box.jsx
@@ -8,6 +8,7 @@ const StyledDiv = styled.div`
     justify-content: center;
     align-items: center;
     margin: 2rem 1rem;
+    //width & height in pixels always
     width: ${props => props.newWidth+"px"};
     height: ${props => props.newHeight+"px"};
     background-color: ${ color => color.newColor};
@@ -21,46 +22,29 @@ const Box = () => {
     //manage the states
     const [state, setState] = useState({
         boxes : [],
-        color: "red",
-        height: 100,
-        width: 100
+        color: "green",
+        height: 300,
+        width: 300
     })
 
     //Se implementa cada onChangeColor, onChangeHeight y onChangeWidth para cada botón Add
 
-    const onChangeColor = (event) => {
-        const {value} = event.target;
-        //const value = event.target.value;
+    const onChange = (event) => {
+        const {name, value} = event.target;
         setState({
             ...state,
-            color: value
+            [name] : value
         })
     }
 
-    const onChangeHeight = (event) => {
-        const {value} = event.target;
-        //const value = event.target.value;
-        setState({
-            ...state,
-            height: value
-        })
-    }
-    const onChangeWidth = (event) => {
-        const {value} = event.target;
-        //const value = event.target.value;
-        setState({
-            ...state,
-            width: value
-        })
-    }
     const onSubmit = (event) => {
         event.preventDefault();
         //si no aplico el setState cambiandole otro key y qye reciba el state.color, este se cambia automaticamente con el onChange sin necesidad del onSubmit (hacer click en el botón)
         setState({
             boxes: [...state.boxes, {_color:state.color, _height:state.height, _width:state.width}],
-            color: "red",
-            height: 100,
-            width: 100
+            color: "green",
+            height: 300,
+            width: 300
             
         })
         colorInput.current.value="";
@@ -84,15 +68,15 @@ const Box = () => {
             <form onSubmit={onSubmit}>
                 <div className="row">
                     <label htmlFor="color">Color</label>
-                    <input type="text" ref={colorInput} id="color" name="color" /* value={state.color} */ onChange={onChangeColor} placeholder="Enter the color of the box"/>
+                    <input type="text" ref={colorInput} id="color" name="color" /* value={state.color} */ onChange={onChange} placeholder="Enter the color of the box"/>
                 </div>
                 <div className="row">
                     <label htmlFor="height">Height</label>
-                    <input type="number" id="height" ref={heightInput} name="height" /* value={state.height} */ onChange={onChangeHeight} placeholder="Enter the height of the box"/>
+                    <input type="number" id="height" ref={heightInput} name="height" /* value={state.height} */ onChange={onChange} placeholder="Enter the height of the box"/>
                 </div>
                 <div className="row">
                     <label htmlFor="width">Width</label>
-                    <input type="number" id="width" ref={widthInput} name="width" /* value={state.width} */ onChange={onChangeWidth} placeholder="Enter the width of the box"/>
+                    <input type="number" id="width" ref={widthInput} name="width" /* value={state.width} */ onChange={onChange} placeholder="Enter the width of the box"/>
                 </div>
                <div className="button">
                    <button type="submit">Add</button>
