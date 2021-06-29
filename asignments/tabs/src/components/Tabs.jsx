1072e5fcdcaa08decf0090382000eabea3b68c6f
Stefano <piero.quiroz93@gmail.com>
2021-06-28 19:05:46 -0500

    Added styled-components-buttons

--- asignments/tabs/src/components/Tabs.jsx
+++ asignments/tabs/src/components/Tabs.jsx
@@ -1,4 +1,29 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const TabsDiv = styled.div`
+    //outline: 1px solid green;
+    width: 42%;
+    margin-left: 10%;
+    margin-top: 1%;
+    display: flex;
+    justify-content: space-around;
+`; 
+
+const StyledButton = styled.button`
+    @import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
+    font-family: 'Dancing Script';
+    width: 30%;
+    font-size: 2rem;
+    padding: 0.5rem 0rem;
+    background-color: ${props => (props.backColor) ? "#000" : "#fff"};
+    color: ${props => (props.color) ? "#fff" : "#000"};
+    border-color: ${props => (props.border) ? "#000" : "#efefef"};
+    border-width: 3px; 
+    border-style: solid;
+    box-sizing: border-box;
+`;
+
 
 const Tabs = ({tabs}) => {
     
@@ -14,14 +39,20 @@ const Tabs = ({tabs}) => {
     }
 
     const buttonTabs = tabs.map((item, index) => (
-        <button key={index} onClick={event => onClick(item)}>Tabs {item}</button>
+        <StyledButton 
+            key={index} 
+            onClick={event => onClick(item)}
+            backColor={state.tab === item}
+            color={state.tab === item}
+            border={state.tab === item}
+        >Tabs {item}</StyledButton>
     )) 
 
     return (
         <div className="container">
-            <div className="tabs">
+            <TabsDiv>
                 {buttonTabs}
-            </div>
+            </TabsDiv>
             <div className="content">
                 <div>Tab {state.tab} content is showing here</div>
             </div>            
