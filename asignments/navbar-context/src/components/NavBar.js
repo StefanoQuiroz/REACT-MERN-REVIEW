291601f948e20f3beb87e814cd1738c7c48c4e05
Stefano <piero.quiroz93@gmail.com>
2021-06-30 02:19:33 -0500

    Added styledNav

--- asignments/navbar-context/src/components/NavBar.js
+++ asignments/navbar-context/src/components/NavBar.js
@@ -1,9 +1,27 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { MyContext } from './Wrapper';
+
+const StyledNav = styled.nav`
+    //outline: 1px solid red;
+    background-color: #8c44cf;
+    height: 5rem;
+    width: 90%;
+    box-sizing: border-box;
+    margin: 1rem auto;
+    padding: 1rem;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    font-size: 1.7rem;
+    font-weight: 450;
+    color: #fff;
+`;
+
 const NavBar = () => {
     const {state} = useContext(MyContext)
     return (
-        <nav>Hi {state.name}!!</nav>
+        <StyledNav>Hi {state.name}!</StyledNav>
     );
 }
 
